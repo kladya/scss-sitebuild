14014b200be66ac6c98be6fd4839abdede94f87b
kladya <konkla@gmail.com>
2021-01-19 01:52:20 +0100

    Page finished

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const menuButton = document.querySelector('.nav-button');
 menuButton.addEventListener('click', () => {
     const navList = document.querySelector('.nav-list');
@@ -9,13 +11,28 @@ menuButton.addEventListener('click', () => {
 
 const navHide = () => {
     const navList = document.querySelectorAll('.nav-link');
-    navList.forEach(link => link.addEventListener ('click', () => {
+    navList.forEach(link => link.addEventListener('click', () => {
         const navList = document.querySelector('.nav-list');
         if (navList.style.display == 'none')
             navList.style.display = 'block';
         else
             navList.style.display = 'none';
-    }));        
+    }));
 }
 
 navHide();
+
+const upperLinks = document.querySelectorAll(".nav-link-upper");
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos < currentScrollPos) {
+        document.querySelector(".fixed").style.backgroundColor = "white";
+        upperLinks.forEach(link => link.style.color = "grey");
+    }
+    if (currentScrollPos === 0) {
+        document.querySelector(".fixed").style.removeProperty("background-color");
+        upperLinks.forEach(link => link.style.removeProperty("color"));
+    }
+    prevScrollpos = currentScrollPos;
+}
